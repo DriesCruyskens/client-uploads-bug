1cc7c8cd1e317079d1273c7703cd4ef1432976f8
Dries Cruyskens <dries.cruyskens@gmail.com>
2025-03-16 11:02:20 +0100

    add new-media and 2nd storage plugin with clientUploads enabled

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,12 +4,13 @@ import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
 import { s3Storage } from '@payloadcms/storage-s3'
+import { Media } from './collections/Media'
+import { NewMedia } from './collections/NewMedia'
+import { Users } from './collections/Users'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -21,7 +22,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, NewMedia],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -36,17 +37,34 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     s3Storage({
+      clientUploads: true, // <-- only the first plugin instance that declares clientUploads: true will work.
       collections: {
         media: true,
       },
       bucket: process.env.S3_BUCKET!,
       config: {
-        // credentials: {
-        //   accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        //   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-        // },
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+        },
         region: process.env.S3_REGION,
-        // ... Other S3 configuration
+      },
+    }),
+    // Seconds storage plugin so we can use a different bucket for new-media
+    s3Storage({
+      // Other instances that use clientUploads don't work. I assume because the /storage-s3-generate-signed-url endpoint is already configured?
+      // If we switch these two plugin instance around, the new-media collection will work but the media collection will not.
+      clientUploads: true, // <--- This prevents uploads from working
+      collections: {
+        'new-media': true,
+      },
+      bucket: process.env.S3_NEW_BUCKET!, // <--- Different bucket
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+        },
+        region: process.env.S3_REGION,
       },
     }),
   ],
